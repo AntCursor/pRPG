d7a87bb357381073caa385eca53a486e399643ef
AntCursor <antcursor@gmail.com>
2026-05-13 02:27:45 -0300

    format: formated Hero.java

--- src/character/Hero.java
+++ src/character/Hero.java
@@ -10,61 +10,68 @@ public class Hero extends Character {
 
   private Direction pendingDirection = null;
 
-  private TileType sprite = TileType.LEVI;
+  private TileType sprite = TileType.MAN;
 
   public Hero(String name, int x, int y) {
     super(name, x, y);
-    this.maxHp    = 100;
-    this.hp       = 100;
-    this.speed    = 5;
-    this.power    = 15;
-    this.cooldown = 2000; //2s entre os moves
+    this.maxHp = 100;
+    this.hp = 100;
+    this.speed = 5;
+    this.power = 15;
+    this.cooldown = 2000; // 2s entre os moves
   }
 
   public void queueDirection(int keyCode) {
     switch (keyCode) {
-      case 'W': case 'w': 
-        pendingDirection = Direction.UP;    
-      break;
-      case 'S': case 's': 
-        pendingDirection = Direction.DOWN;  
-      break;
-      case 'A': case 'a': 
-        pendingDirection = Direction.LEFT;  
-      break;
-      case 'D': case 'd': 
-        pendingDirection = Direction.RIGHT; 
-      break;
-      default: 
-      break;
+      case 'W':
+      case 'w':
+        pendingDirection = Direction.UP;
+        break;
+      case 'S':
+      case 's':
+        pendingDirection = Direction.DOWN;
+        break;
+      case 'A':
+      case 'a':
+        pendingDirection = Direction.LEFT;
+        break;
+      case 'D':
+      case 'd':
+        pendingDirection = Direction.RIGHT;
+        break;
+      default:
+        break;
     }
   }
 
   @Override
   public void update(GameContext game, Grid grid) {
-    if (pendingDirection == null) return;
+    if (pendingDirection == null)
+      return;
 
     Direction dir = pendingDirection;
     pendingDirection = null;
 
     boolean cooldownOk = (game.millis() - lastActionTime) >= cooldown;
 
-    if (!cooldownOk) return;
-    if (!canMove(dir, grid)) return;
+    if (!cooldownOk)
+      return;
+    if (!canMove(dir, grid))
+      return;
 
     switch (dir) {
       case UP:
         y -= 1;
-      break;
+        break;
       case DOWN:
         y += 1;
-      break;
+        break;
       case LEFT:
-        x -= 1; 
-      break;
+        x -= 1;
+        break;
       case RIGHT:
-        x += 1; 
-      break;
+        x += 1;
+        break;
     }
     lastActionTime = game.millis();
   }
@@ -80,4 +87,3 @@ public class Hero extends Character {
     return this;
   }
 }
-
